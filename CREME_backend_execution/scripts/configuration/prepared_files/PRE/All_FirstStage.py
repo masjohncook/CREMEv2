5bd10d65f10dfa7a57d6f62e45001b096a97b695
masjohncook <mas.john.cook@gmail.com>
2022-01-31 06:49:12 -0500

    add nmap function and add new directory structure

--- CREME_backend_execution/scripts/configuration/prepared_files/PRE/All_FirstStage.py
+++ CREME_backend_execution/scripts/configuration/prepared_files/PRE/All_FirstStage.py
@@ -1,11 +1,27 @@
-import time, os, sys
+import time, subprocess, os, sys
 from nmap import nmap
 
+
 def ip_scan():
-    nm = nmap.PortScanner('192.168.1.0/24')
-    nm.all_hosts()
+    nbt = "nbtscan"
+    nm = nmap.PortScanner()
+    nm.scan('192.168.56.0/24', '0-65535')
+    host_list = nm.all_hosts()
+    print("====================List of Target====================")
+    for x in host_list:
+        state = nm[x].state()
+        nbt_command = nbt+" -e " + x #+ " |awk '{print $2}'"
+        nbt_scan = str(os.system(nbt_command))
+        print(x + " : " + nbt_scan)
+        # if nbt_scan == 0:
+        #     print(x + " : " + state + " --> Netbios name was not discovered")
+        # else:
+        #     print(x + " : " + state + " -->" + str(nbt_scan))
+
+
+def netbios_scan():
+    print()
 
 
 if __name__ == "__main__":
     ip_scan()
-
