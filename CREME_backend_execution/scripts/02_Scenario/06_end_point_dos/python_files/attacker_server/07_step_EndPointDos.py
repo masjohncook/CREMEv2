726859f09a004e8a0478159bbdbe56ab5ab3b7b3
masjohncook <mas.john.cook@gmail.com>
2022-02-17 20:27:06 -0500

    feat - Add some logic to the End Point Dos scenario

--- CREME_backend_execution/scripts/02_Scenario/06_end_point_dos/python_files/attacker_server/07_step_EndPointDos.py
+++ CREME_backend_execution/scripts/02_Scenario/06_end_point_dos/python_files/attacker_server/07_step_EndPointDos.py
@@ -2,3 +2,9 @@ import time
 import sys
 import os
 from pymetasploit3.msfrpc import MsfRpcClient
+
+
+def record_timestamp(folder, output_time_file):
+    output_time_file = os.path.join(folder, output_time_file)
+    with open(output_time_file, "w+") as fw:
+        fw.write('%f' % time.time())
--- CREME_backend_execution/scripts/02_Scenario/06_end_point_dos/python_files/attacker_server/08_step_EndPointDos.py
+++
@@ -1,4 +0,0 @@
-import time
-import sys
-import os
-from pymetasploit3.msfrpc import MsfRpcClient
