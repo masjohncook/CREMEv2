cf3110aba2cb26ac96200fefeb218d39da35bc33
masjohncook <mas.john.cook@gmail.com>
2022-02-28 08:02:52 -0500

    feat - update new end point dos with slowloris attack

--- setup_tools.sh
+++ setup_tools.sh
@@ -3,7 +3,7 @@
 # install python 3.6
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install python3.6 -y
+sudo apt-get install python3.5 -y
 
 sudo apt-get update
 sudo apt-get install expect -y
