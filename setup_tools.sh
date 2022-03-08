d88a620e1fbfc2fa3c587d8618b539f5d5ffbf2a
masjohncook <48942450+masjohncook@users.noreply.github.com>
2022-03-08 16:49:20 +0800

    Update setup_tools.sh

--- setup_tools.sh
+++ setup_tools.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 sudo apt-get install expect -y
 
 # virtual environment
-sudo apt-get install python-virtualenv -y
+sudo apt-get install python3-venv -y
 
 # install ssh server
 sudo apt-get install openssh-server -y
