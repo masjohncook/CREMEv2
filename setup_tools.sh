abcb6690e208c274c762cc732ec76216867c0db9
shinyee <55288000+charlie532@users.noreply.github.com>
2022-03-10 12:01:03 +0800

    fix: update installation method of Python3.6.2

--- setup_tools.sh
+++ setup_tools.sh
@@ -9,6 +9,11 @@ cd Python-3.6.2/
 ./configure
 sudo make altinstall
 cd ..
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 2
+sudo update-alternatives --install /usr/bin/python python /usr/local/bin/python3.6 2
+
+
 
 sudo apt-get update
 sudo apt-get install expect -y
