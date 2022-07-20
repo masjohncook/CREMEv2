0f5727c098efe47daabcb2c06c82834ce84de434
AlexHchs <88889955+AlexHchs@users.noreply.github.com>
2022-07-20 22:48:12 +0800

    Update 03_step_AttackerServer_Ransomware.sh

--- CREME_backend_execution/scripts/02_Scenario/03_ransomware/bash_files/03_step_AttackerServer_Ransomware.sh
+++ CREME_backend_execution/scripts/02_Scenario/03_ransomware/bash_files/03_step_AttackerServer_Ransomware.sh
@@ -19,7 +19,7 @@ send "$password\r"
 set timeout 60
 
 expect "*:~# "
-send "python3 $path/3rd_step_Ransomware.py $path $ip $target_server_ip\r"
+send "python3 $path/03_step_Ransomware.py $path $ip $target_server_ip\r"
 
 expect "*:~# "
 send "exit\r"
