e544f8762f40161f707b003d145486cd706634d9
AlexHchs <88889955+AlexHchs@users.noreply.github.com>
2022-07-23 15:20:00 +0800

    Update 07_step_DiskWipe.py

--- CREME_backend_execution/scripts/02_Scenario/05_disk_wipe/python_files/attacker_server/07_step_DiskWipe.py
+++ CREME_backend_execution/scripts/02_Scenario/05_disk_wipe/python_files/attacker_server/07_step_DiskWipe.py
@@ -21,7 +21,7 @@ def main(argv):
     client = MsfRpcClient('kali')
 
     # start step 7
-    output_time_file = 'time_step_7_start.txt'
+    output_time_file = 'time_stage_7_start.txt'
     record_timestamp(folder, output_time_file)
     time.sleep(2)
 
