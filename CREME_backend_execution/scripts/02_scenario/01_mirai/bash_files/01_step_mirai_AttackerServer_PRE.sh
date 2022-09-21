9e6c09e6c35378437ce3a2f51bf1c7cd64589369
masjohncook <mas.john.cook@gmail.com>
2022-09-21 18:56:44 +0800

    dev - add first and second steps to mirai

--- CREME_backend_execution/scripts/02_scenario/01_mirai/bash_files/01_step_mirai_AttackerServer_PRE.sh
+++ CREME_backend_execution/scripts/02_scenario/01_mirai/bash_files/01_step_mirai_AttackerServer_PRE.sh
@@ -19,7 +19,7 @@ send "$password\r"
 set timeout 60
 
 expect "*:~# "
-send "python3 $path/0_step_PRE_Mirai.py $path $ip $target_server_ip\r"
+send "python3 $path/01_step_PRE_Mirai.py $path $ip $target_server_ip\r"
 
 expect "*:~# "
 send "exit\r"
