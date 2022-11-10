992d22d401f56f66eb3da0ce2f75e0c5b5b9b3f8
masjohncook <mas.john.cook@gmail.com>
2022-11-10 21:05:05 +0800

    fix - update time sleep to add times

--- CREME_backend_execution/scripts/02_scenario/02_disk_wipe/python_files/attacker_server/05_step_DiskWipe.py
+++ CREME_backend_execution/scripts/02_scenario/02_disk_wipe/python_files/attacker_server/05_step_DiskWipe.py
@@ -31,9 +31,8 @@ def main(argv):
     exploit.execute(payload=payload)
 
     while client.jobs.list:
-        time.sleep(1)
+        time.sleep(20)
 
-    time.sleep(30)
     output_time_file_end = 'time_step_5_end.txt'
     record_timestamp(folder, output_time_file_end)
     time.sleep(30)
