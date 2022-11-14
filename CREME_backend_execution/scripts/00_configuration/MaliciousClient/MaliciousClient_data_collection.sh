1ed0c935ddb9a2aa04673f7fc241041829b9f6f1
masjohncook <mas.john.cook@gmail.com>
2022-11-14 11:26:50 +0800

    fix - update timzone configuration

--- CREME_backend_execution/scripts/00_configuration/MaliciousClient/MaliciousClient_data_collection.sh
+++ CREME_backend_execution/scripts/00_configuration/MaliciousClient/MaliciousClient_data_collection.sh
@@ -30,12 +30,9 @@ expect "*:~# "
 send "iptables-save > /etc/iptables/rules.v4\r"
 
 ## update time
-#expect "*:~# "
-#send "systemctl stop ntp\r"
-#expect "*:~# "
-#send "sudo ntpdate ntp.ubuntu.com\r"
-#expect "*:~# "
-#send "systemctl restart ntp\r"
+expect "*:~# "
+send "timedatectl set-timezone Asia/Taipei\r"
+set timeout 60
 
 # exit
 expect "*:~# "
