0cacb918db005f11bada03e4f33ffadfbf874a71
Charlie532 <402057@gmail.com>
2022-11-14 17:56:06 +0800

    delete redundant parameters 4

--- CREME_backend_execution/scripts/03_Preprocessing/Accounting/auto_accounting_process.sh
+++ CREME_backend_execution/scripts/03_Preprocessing/Accounting/auto_accounting_process.sh
@@ -10,26 +10,26 @@ location=$1
 code_path=$3
 
 filename=$(basename $2 .raw)
-raw_disk_file="${filename}_disk.txt"
-raw_memory_file="${filename}_memory.txt"
-raw_process_file="${filename}_process.txt"
+raw_disk_file="${location}/${filename}_disk.txt"
+raw_memory_file="${location}/${filename}_memory.txt"
+raw_process_file="${location}/${filename}_process.txt"
 csv_disk_file="${location}/${filename}_disk.csv"
 csv_memory_file="${location}/${filename}_memory.csv"
 csv_process_file="${location}/${filename}_process.csv"
 csv_merge="${location}/${filename}_merge.csv"
 
 # atop.raw -> .txt
-atop -d -r "${location}/${2}" > "${location}/${raw_disk_file}"
-atop -m -r "${location}/${2}" > "${location}/${raw_memory_file}"
-atop -s -r "${location}/${2}" > "${location}/${raw_process_file}"
+atop -d -r "${location}/${2}" > $raw_disk_file
+atop -m -r "${location}/${2}" > $raw_memory_file
+atop -s -r "${location}/${2}" > $raw_process_file
 
 # .txt -> .csv
-python3 ${code_path}/extract_atop.py "${location}/${raw_disk_file}" $csv_disk_file 1
-python3 ${code_path}/extract_atop.py "${location}/${raw_memory_file}" $csv_memory_file 1
-python3 ${code_path}/extract_atop.py "${location}/${raw_process_file}" $csv_process_file 1
+python3 ${code_path}/extract_atop.py $raw_disk_file $csv_disk_file 1
+python3 ${code_path}/extract_atop.py $raw_memory_file $csv_memory_file 1
+python3 ${code_path}/extract_atop.py $raw_process_file $csv_process_file 1
 
 # merge .csv
 python3 ${code_path}/merge_atop.py $csv_disk_file $csv_memory_file $csv_process_file $csv_merge
 
 # remove intermediate result
-rm "${location}/${raw_disk_file}" "${location}/${raw_memory_file}" "${location}/${raw_process_file}" $csv_disk_file $csv_memory_file $csv_process_file
+rm $raw_disk_file $raw_memory_file $raw_process_file $csv_disk_file $csv_memory_file $csv_process_file
