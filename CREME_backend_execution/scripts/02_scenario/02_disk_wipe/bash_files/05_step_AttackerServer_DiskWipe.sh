708f2044c0cf0bcebca44b73e8530df0afaceff4
masjohncook <mas.john.cook@gmail.com>
2022-11-28 18:11:19 +0800

    fix - update timeout

--- CREME_backend_execution/scripts/02_scenario/02_disk_wipe/bash_files/05_step_AttackerServer_DiskWipe.sh
+++ CREME_backend_execution/scripts/02_scenario/02_disk_wipe/bash_files/05_step_AttackerServer_DiskWipe.sh
@@ -6,7 +6,7 @@ set password [lindex $argv 3]
 set path [lindex $argv 4]
 set target_server_ip [lindex $argv 5]
 
-set timeout 900
+set timeout 600
 
 # SSH connection
 spawn /bin/bash $delKnownHosts
