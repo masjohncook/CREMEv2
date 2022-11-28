cf873c3abef39804af1cea54254933472dd6c89a
masjohncook <mas.john.cook@gmail.com>
2022-11-28 17:52:41 +0800

    fix - move the timestamp generation

--- CREME_backend_execution/scripts/02_scenario/04_resource_hijacking/python_files/attacker_server/03_step_ResourceHijacking.py
+++ CREME_backend_execution/scripts/02_scenario/04_resource_hijacking/python_files/attacker_server/03_step_ResourceHijacking.py
@@ -18,7 +18,7 @@ def main(argv):
     my_ip = argv[2]
     target_ip = argv[3]
     
-    output_time_file_start = 'time_step_4_start.txt'
+    output_time_file_start = 'time_step_3_start.txt'
     record_timestamp(folder, output_time_file_start)
     time.sleep(2)
 
@@ -35,7 +35,7 @@ def main(argv):
         time.sleep(1)
 
     time.sleep(30)
-    output_time_file_end = 'time_step_4_end.txt'
+    output_time_file_end = 'time_step_3_end.txt'
     record_timestamp(folder, output_time_file_end)
     time.sleep(30)
 
