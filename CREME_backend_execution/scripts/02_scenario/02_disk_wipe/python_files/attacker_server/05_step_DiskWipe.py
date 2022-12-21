291e019b2f19851b4418df1f9b3fc846ceba76d9
masjohncook <mas.john.cook@gmail.com>
2022-12-21 17:22:35 +0800

    fix - disk wipe timestamp problem

--- CREME_backend_execution/scripts/02_scenario/02_disk_wipe/python_files/attacker_server/05_step_DiskWipe.py
+++ CREME_backend_execution/scripts/02_scenario/02_disk_wipe/python_files/attacker_server/05_step_DiskWipe.py
@@ -19,12 +19,6 @@ def main(argv):
 
     client = MsfRpcClient('kali')
 
-
-    output_time_file_end = 'time_step_4_end.txt'
-    record_timestamp(folder, output_time_file_end)
-    time.sleep(30)
-
-
     # start step 6
     output_time_file_start = 'time_step_5_start.txt'
     record_timestamp(folder, output_time_file_start)
