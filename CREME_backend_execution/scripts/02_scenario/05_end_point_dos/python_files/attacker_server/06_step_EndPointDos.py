7358b5b22aa5e95b6a9f206418e0278ef014aea3
masjohncook <mas.john.cook@gmail.com>
2023-01-10 11:04:58 +0800

    dev - ~

--- CREME_backend_execution/scripts/02_scenario/05_end_point_dos/python_files/attacker_server/06_step_EndPointDos.py
+++ CREME_backend_execution/scripts/02_scenario/05_end_point_dos/python_files/attacker_server/06_step_EndPointDos.py
@@ -11,12 +11,6 @@ def record_timestamp(folder, output_time_file):
 
 
 def main(argv):
-    time.sleep(30)
-    output_time_file_end = 'time_step_5_end.txt'
-    record_timestamp(folder, output_time_file_end)
-    time.sleep(30)
-    
-    
     folder = argv[1]
     my_ip = argv[2]
     target_ip = argv[3]
